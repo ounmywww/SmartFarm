2cc7cefb00bf802dd60eee9d05b3908e8a4b9ab7
macbook <ounmywww@naver.com>
2020-04-30 16:42:04 +0900

    Test Commit

--- app/src/main/java/smartfarm/com/smartfarm/SocketHelper.java
+++ app/src/main/java/smartfarm/com/smartfarm/SocketHelper.java
@@ -1,10 +1,18 @@
 package smartfarm.com.smartfarm;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.net.SocketAddress;
+import java.net.UnknownHostException;
 
 public class SocketHelper {
     String hostname;
     int port;
+    SocketAddress addr;
     Socket socket;
 
     SocketHelper(String hostname, int port){
@@ -13,6 +21,28 @@ public class SocketHelper {
     }
 
     public void getSocket() {
+        try{
+            socket = new Socket();
+            addr = new InetSocketAddress(hostname, port);
+            socket.connect(addr);
+
+            System.out.println("[" + socket.getInetAddress() + "] Client connected");
+
+            OutputStream output = socket.getOutputStream();
+            PrintWriter writer = new PrintWriter(output, true);
+
+            writer.println("XXX");
+
+            InputStream input = socket.getInputStream();
+            int n = input.read();
+
+        }catch (UnknownHostException ex){
+            System.out.println("Server Not Found: " + ex.getMessage());
+        }
+        catch(IOException ex){
+            System.out.println("I/O Err: " + ex.getMessage());
+        }
+
 
     }
 }
