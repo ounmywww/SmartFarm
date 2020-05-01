b9369ff47343b91f7f8841eb2ed4e9dfa6f46c46
macbook <ounmywww@naver.com>
2020-05-01 16:03:58 +0900

    Test Commit

--- app/src/main/java/smartfarm/com/smartfarm/MainActivity.java
+++ app/src/main/java/smartfarm/com/smartfarm/MainActivity.java
@@ -1,13 +1,31 @@
 package smartfarm.com.smartfarm;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private SocketHelper sh;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sh = new SocketHelper();
+        sh.start();
+    }
+
+    public void onButtonClick(View view){
+        TextView textView1 = (TextView) findViewById(R.id.TvTemp);
+        switch (view.getId()) {
+            case R.id.ButtonOpen :
+                sh.sendMassage("Open");
+                break ;
+            case R.id.ButtonClose :
+                sh.sendMassage("Close");
+                break ;
+        }
     }
 }
--- app/src/main/java/smartfarm/com/smartfarm/SocketHelper.java
+++ app/src/main/java/smartfarm/com/smartfarm/SocketHelper.java
@@ -9,40 +9,70 @@ import java.net.Socket;
 import java.net.SocketAddress;
 import java.net.UnknownHostException;
 
-public class SocketHelper {
+public class SocketHelper extends Thread{
     String hostname;
     int port;
     SocketAddress addr;
     Socket socket;
+    OutputStream output;
+    InputStream input;
+    PrintWriter writer;
 
-    SocketHelper(String hostname, int port){
+    public SocketHelper(){
+        this.hostname = "172.30.1.18";
+        this.port = 9000;
+    }
+
+    public SocketHelper(String hostname, int port){
         this.hostname = hostname;
         this.port = port;
     }
 
-    public void getSocket() {
+    private void finallize(){
         try{
-            socket = new Socket();
-            addr = new InetSocketAddress(hostname, port);
-            socket.connect(addr);
+            if(output != null){
+                output.close();
+            }
+            if(input != null){
+                output.close();
+            }
+            if(socket != null){
+                socket.close();
+            }
+        }catch(IOException ex){
+            System.out.println("I/O Err: " + ex.getMessage());
+        }
+
+
+    }
+
+    public void run() {
+        try{
+            this.socket = new Socket();
+            this.addr = new InetSocketAddress(hostname, port);
+            this.socket.connect(addr);
 
             System.out.println("[" + socket.getInetAddress() + "] Client connected");
 
-            OutputStream output = socket.getOutputStream();
-            PrintWriter writer = new PrintWriter(output, true);
+            this.output = socket.getOutputStream();
+            this.writer = new PrintWriter(output, true);
 
-            writer.println("XXX");
+            writer.println("Client Access");
 
-            InputStream input = socket.getInputStream();
-            int n = input.read();
+            this.input = socket.getInputStream();
+            //int n = input.read();
 
         }catch (UnknownHostException ex){
             System.out.println("Server Not Found: " + ex.getMessage());
         }
         catch(IOException ex){
             System.out.println("I/O Err: " + ex.getMessage());
+        }catch(Exception ex){
+            System.out.println("Exceptio: " + ex.getMessage());
         }
+    }
 
-
+    public void sendMassage(String s){
+        writer.println(s);
     }
 }
