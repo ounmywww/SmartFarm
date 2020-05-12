9cde88f31471d8e54c0d2853ee9e2f38f735637d
macbook <ounmywww@naver.com>
2020-05-12 21:54:25 +0900

    go

---
+++ app/src/main/java/smartfarm/com/smartfarm/DbHelper.java
@@ -0,0 +1,8 @@
+package smartfarm.com.smartfarm;
+
+/**
+ * Created by macbook on 12/05/2020.
+ */
+
+public class DbHelper {
+}
--- app/src/main/java/smartfarm/com/smartfarm/MainActivity.java
+++ app/src/main/java/smartfarm/com/smartfarm/MainActivity.java
@@ -3,7 +3,7 @@ package smartfarm.com.smartfarm;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
 
         sh = new SocketHelper();
         sh.start();
+
+        setTabText();
 
         getTemperThread();
     }
@@ -31,8 +33,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public int byteArrayToInt(byte [] b, int startIndex) {
-        int n1 = b[startIndex] & 0xFF;
-        int n2 = (b[startIndex + 1] & 0xFF) << 8;
+        int n1 = (b[startIndex    ] & 0xFF);
+        int n2 = (b[startIndex + 1] & 0xFF) <<  8;
         int n3 = (b[startIndex + 2] & 0xFF) << 16;
         int n4 = (b[startIndex + 3] & 0xFF) << 24;
 
@@ -57,9 +59,9 @@ public class MainActivity extends AppCompatActivity {
 
                         byte[] bytes = sh.getMassage();
 
-                        int n1 = byteArrayToInt(bytes, 0);
-                        int n2 = byteArrayToInt(bytes, 4);
-                        int n3 = byteArrayToInt(bytes, 8);
+                        int n1 = byteArrayToInt(bytes,  0);
+                        int n2 = byteArrayToInt(bytes,  4);
+                        int n3 = byteArrayToInt(bytes,  8);
                         int n4 = byteArrayToInt(bytes, 12);
 
                         /*int n1 = sh.getMassageInt();
@@ -69,13 +71,44 @@ public class MainActivity extends AppCompatActivity {
 
                         System.out.println("n = " + n1);
 
-                        TextView tv = (TextView)findViewById(R.id.TvTemp);
+                        TextView tvCal   = (TextView)findViewById(R.id.tvCal);
+                        TextView tvWater = (TextView)findViewById(R.id.tvWater);
 
-                        tv.setText("현재 습도 : "+ Integer.toString(n1) + "." + Integer.toString(n2) + "\n"
-                                  + "현재 온도 : " + Integer.toString(n3) + "." + Integer.toString(n4) + "\n");
+                        tvCal.setText(  "현재 습도 : " + Integer.toString(n1) + "." + Integer.toString(n2));
+                        tvWater.setText("현재 온도 : " + Integer.toString(n3) + "." + Integer.toString(n4));
                     }
                 });
             }
         }).start();
+    }
+
+    public void setTabText(){
+        TabHost tabHost1 = (TabHost) findViewById(R.id.tabHost1) ;
+        tabHost1.setup() ;
+
+        // 첫 번째 Tab. (탭 표시 텍스트:"TAB 1"), (페이지 뷰:"content1")
+        TabHost.TabSpec ts1 = tabHost1.newTabSpec("Tab Spec 1") ;
+        ts1.setContent(R.id.content1) ;
+        ts1.setIndicator("Control") ;
+        tabHost1.addTab(ts1)  ;
+
+        // 두 번째 Tab. (탭 표시 텍스트:"TAB 2"), (페이지 뷰:"content2")
+        TabHost.TabSpec ts2 = tabHost1.newTabSpec("Tab Spec 2") ;
+        ts2.setContent(R.id.content2) ;
+        ts2.setIndicator("Graph") ;
+        tabHost1.addTab(ts2) ;
+
+        // 세 번째 Tab. (탭 표시 텍스트:"TAB 3"), (페이지 뷰:"content3")
+        TabHost.TabSpec ts3 = tabHost1.newTabSpec("Tab Spec 3") ;
+        ts3.setContent(R.id.content3) ;
+        ts3.setIndicator("Settings") ;
+        tabHost1.addTab(ts3) ;
+    }
+
+    public void setNumPicker(){
+        NumberPicker npCal = (NumberPicker) findViewById(R.id.npCal);
+
+        npCal.setMinValue(0);
+        npCal.setMinValue(50);
     }
 }
